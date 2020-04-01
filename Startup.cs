00c9c668e8c5dcb03d559c81ff4e0977adf0348e
Raphaël Désalbres <raphael.desalbres@outlook.com>
2020-04-01 22:45:57 +0200

    Update to .NET Core 3.1

--- Startup.cs
+++ Startup.cs
@@ -18,8 +18,7 @@ namespace FamilyHierarchy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddServerSideBlazor();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
